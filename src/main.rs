f16907e87d92a52fb8c511cd97c6dbe0f47166f5
huud-e <huud.duu@gmail.com>
2022-04-13 18:05:47 +0200

    previus to redis incorporation

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,16 @@ use rocket::response::Redirect;
 use rocket::http::{Cookie, CookieJar};
 use rocket_dyn_templates::Template;
 use std::collections::HashMap;
-use rocket_sync_db_pools::{database};
+// use rocket_sync_db_pools::{database};
 use serde_json::json;
 
-#[macro_use]
-extern crate rocket;
+#[macro_use] extern crate rocket;
 /*
 #[macro_use]
 extern crate diesel;
 */
 
-#[macro_use]
-extern crate rocket_include_static_resources;
+#[macro_use] extern crate rocket_include_static_resources;
 
 mod vpt;
 use vpt::*;
@@ -21,59 +19,61 @@ use vpt::*;
 static_response_handler! {
     "/favicon.ico" => favicon => "favicon",
 }
-
-#[database("stocks-db")]
+/*
+#[database("rocket-db")]
 struct StocksDbConn(diesel::PgConnection);
-
+ */
 #[get("/")]
-fn index() -> Template {
-    let context: HashMap<&str, &str> = HashMap::new();
-    Template::render("index", &context)
-}
-
-#[get("/api/<stock>")]
-async fn stock(stock: String) -> Result<Template, std::io::Error> {
-
-    let body = reqwest::get(url(stock.to_owned())).await.unwrap().text().await.unwrap();
-    let parsed = json!(&body);
-    let mut target: HashMap<String,String> = HashMap::new();
+async fn index(cookies: &CookieJar<'_> ) -> Result<Template, std::io::Error> {
+    let mut target: HashMap<String, Vec<f32>> = HashMap::new();
     
-    match volumepricetrend(body.to_owned()) {
-        Err(e) => println!("{:?}", e),
-        _ => ()
+    for c in cookies.iter() {
+        let body = reqwest::get(url(c.name().to_string())).await.unwrap().text().await.unwrap();
+        target.insert(format!("{}", c.name()), analize(body));
     }
-    if parsed == "{\n    \"Error Message\": \"Invalid API call. Please retry or visit the documentation (https://www.alphavantage.co/documentation/) for TIME_SERIES_DAILY.\"\n}"{
-        println!("Error!");
-        // context.insert("Error".to_string(), format!("Stock: {}, not found in API!", &stock));
-        let context: String = format!("Error, stock: {}, doesnt exist in API!", stock);
-        target.insert("e".to_string(), context);
-        Ok(Template::render("stock", target))
-        
-    }else{
-        println!("Todo bien!");
-        let context: String = format!("Good, stock: {}, exists in API!", stock);
-        target.insert("v".to_string(), context);
-        Ok(Template::render("stock", target))
-    }
+    let mut vec: HashMap<String, HashMap<String, Vec<f32>>> = HashMap::new();
+    vec.insert(format!("stocks"), target);
+
+    Ok(Template::render("index", vec))
 }
+
 
 #[post("/", format="application/x-www-form-urlencoded",data="<stock>")]
-fn submit(stock: String, cookies: &CookieJar<'_>, _conn: StocksDbConn) -> Redirect {
+async fn submit(stock: String, cookies: &CookieJar<'_> ) -> Result<Redirect,std::io::Error> {
+    // _conn: StocksDbConn
     let strings: Vec<&str> = stock.split("name=").collect();
     let stock: &str = strings[1];
-    cookies.add(Cookie::new(format!("{}",stock),format!("{}",stock)));
-    Redirect::to(uri!(stock(stock)))
+
+    let mut bool = false;
+    for c in cookies.iter(){
+        if c.name().to_string() == stock{
+            bool = true;
+        }
+    }
+    if bool == false{
+        let body = reqwest::get(url(stock.to_owned())).await.unwrap().text().await.unwrap();
+        let parsed = json!(&body);
+        let mut target: HashMap<String,String> = HashMap::new();
+        if parsed == "{\n    \"Error Message\": \"Invalid API call. Please retry or visit the documentation (https://www.alphavantage.co/documentation/) for TIME_SERIES_DAILY.\"\n}"{
+            println!("Invalid API CALL");
+        }else{
+            cookies.add(Cookie::new(format!("{}", stock),format!("{}", stock)));
+            let context: String = format!("Good, stock: {}, exists in API!", stock);
+            target.insert("v".to_string(), context);
+            println!("Valid API CALL"); 
+        }
+    }
+    Ok(Redirect::to(uri!(index)))
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(Template::fairing())
-        .attach(StocksDbConn::fairing())
+        // .attach(StocksDbConn::fairing())
         .attach(static_resources_initializer!(
             "favicon" => "static/favicon.ico",
         ))
         .mount("/", routes![favicon])
-        .mount("/", routes![index, stock, submit])
-
+        .mount("/", routes![index, submit])
 }
--- src/vpt.rs
+++ src/vpt.rs
@@ -8,8 +8,17 @@
 use dotenv::dotenv;
 use std::env;
 use serde_json::{Result, Value};
+/*
 use std::fs::File;                                                                                                                                                                   
 use std::io::Write;                                                                                                                                                                  
+ */
+use std::{error::Error, time::Duration};
+use tokio::time::sleep;
+use redis::{
+    from_redis_value,
+    streams::{StreamRangeReply, StreamReadOptions, StreamReadReply},
+    AsyncCommands, Client,
+};
 
 fn return_stock_values(v: Value, opt: u8) -> Vec<serde_json::value::Value> {
     let mut vec: Vec<serde_json::value::Value> = Vec::new();
@@ -28,20 +37,25 @@ fn return_stock_values(v: Value, opt: u8) -> Vec<serde_json::value::Value> {
     }
     return vec;
 }
+pub fn url(stock: String) -> String {
+    dotenv().ok();
+    let api_key = env::var("API_KEY")
+        .expect("API_KEY must be set");
+    let url: String = format!("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={URL}&outputsize=full&apikey=", URL= stock);
+    let url = url.to_owned();
+    let key: String = api_key.to_owned();
+    let furl = format!("{}{}", url,key);
+    return furl;
+}
 
-pub fn volumepricetrend(stock: String) -> Result<()> {
+fn volume_price_trend(stock: &String) -> Result<f32> {
     // VPT = Volume x (Today’s Closing Price – Previous Closing Price) / Previous Closing Price
     let v: Value = serde_json::from_str(&stock)?;
     
     
-    let mut vol: Vec<serde_json::value::Value> = Vec::new(); 
-    vol = return_stock_values(v.to_owned(), 5);
-    
-    let mut clo: Vec<serde_json::value::Value> = Vec::new(); 
-    clo = return_stock_values(v.to_owned(), 4);
-    
-    let mut pclo: Vec<serde_json::value::Value> = Vec::new(); 
-    pclo = return_stock_values(v, 4);
+    let vol = return_stock_values(v.to_owned(), 5);   
+    let clo = return_stock_values(v.to_owned(), 4);
+    let pclo = return_stock_values(v, 4);
 
     
     let mut i = vol.len() - 2;
@@ -75,41 +89,76 @@ pub fn volumepricetrend(stock: String) -> Result<()> {
         
     }
 
-
     println!("VPT of stock is {:?}", vpt);
     
-    let mut f = File::create("tmp/vpts.txt").expect("Unable to create file");
-    let mut f2 = File::create("tmp/price.txt").expect("Unable to create file");
-    let mut f3 = File::create("tmp/vol.txt").expect("Unable to create file");
+    Ok(vpt)
+}
 
-    for i in &vpts{
-        let mut tmp = String::from(i.to_string());
-        tmp += "\n";
-        f.write(tmp.to_string().as_bytes()).unwrap();
-    }
-    for i in &vol{
-        let mut tmp = String::from(i.to_string());
-        tmp += "\n";
-        f3.write(tmp.to_string().as_bytes()).unwrap();
-    }
-    for i in &clo{
-        let mut tmp = String::from(i.to_string());
-        tmp += "\n";
-        f2.write(tmp.to_string().as_bytes()).unwrap();
-    }
-    
-    // println!("{:#?}", _vec.len());
+async fn redis() -> Result<()> {
     Ok(())
 }
 
+fn on_balance_volume(stock: &String) -> Result<f32>{
+    let v: Value = serde_json::from_str(&stock)?;
+    
+    let vol = return_stock_values(v.to_owned(), 5);   
+    let clo = return_stock_values(v.to_owned(), 4);
+    let pclo = return_stock_values(v, 4);
 
-pub fn url(stock: String) -> String {
-    dotenv().ok();
-    let api_key = env::var("API_KEY")
-        .expect("API_KEY must be set");
-    let url: String = format!("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={URL}&outputsize=full&apikey=", URL= stock);
-    let url = url.to_owned();
-    let key: String = api_key.to_owned();
-    let furl = format!("{}{}", url,key);
-    return furl;
+    let mut i = vol.len() - 2;
+    let mut ii = vol.len() - 1;
+    
+    let mut obv = 0.0;
+    let mut pobv = 0.0;
+
+    let mut fobv = Vec::new(); 
+    
+    while i > 4{
+
+        // println!("{}{}{}",vol[i], clo[i], i);
+        let vol = vol[i].as_str().unwrap();
+        let vol: f32 = vol.parse().unwrap();
+
+        let clo = clo[i].as_str().unwrap();
+        let clo: f32 = clo.parse().unwrap();
+        
+        let pclo = pclo[ii].as_str().unwrap();
+        let pclo: f32 = pclo.parse().unwrap();
+        
+        
+        if clo > pclo {
+            obv = pobv + vol;
+        }else if clo < pclo {
+            obv = pobv - vol;
+        }else if clo == pclo{
+            obv = pobv;
+        }
+
+        pobv = obv;
+        fobv.push(obv);
+
+        i = i - 1;
+        ii = ii - 1;
+        
+    }
+    println!("OBV of stock is {:?}", obv);
+    Ok(obv)
+
+}
+
+
+pub fn analize(stock:String) -> Vec<f32>{
+    let mut result= Vec::new();
+    
+    let vpt = volume_price_trend(&stock).unwrap();
+    result.push(vpt);
+    
+    let obv = on_balance_volume(&stock).unwrap();
+    result.push(obv);
+    
+
+
+
+
+    return result;
 }

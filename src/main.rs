1044698ad7f068dddc8d0d4802109c8862fa784f
huud-e <huud.duu@gmail.com>
2022-04-16 18:37:24 +0200

    removal of diesel dependencies

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,9 @@ use rocket::response::Redirect;
 use rocket::http::{Cookie, CookieJar};
 use rocket_dyn_templates::Template;
 use std::collections::HashMap;
-// use rocket_sync_db_pools::{database};
 use serde_json::json;
 
 #[macro_use] extern crate rocket;
-/*
-#[macro_use]
-extern crate diesel;
-*/
-
 #[macro_use] extern crate rocket_include_static_resources;
 
 mod vpt;
@@ -19,10 +13,7 @@ use vpt::*;
 static_response_handler! {
     "/favicon.ico" => favicon => "favicon",
 }
-/*
-#[database("rocket-db")]
-struct StocksDbConn(diesel::PgConnection);
- */
+
 #[get("/")]
 async fn index(cookies: &CookieJar<'_> ) -> Result<Template, std::io::Error> {
     let mut target: HashMap<String, Vec<String>> = HashMap::new();
@@ -50,7 +41,6 @@ async fn index(cookies: &CookieJar<'_> ) -> Result<Template, std::io::Error> {
 
 #[post("/", format="application/x-www-form-urlencoded",data="<stock>")]
 async fn submit(stock: String, cookies: &CookieJar<'_> ) -> Result<Redirect,std::io::Error> {
-    // _conn: StocksDbConn
     let strings: Vec<&str> = stock.split("name=").collect();
     let stock: &str = strings[1];
 
@@ -92,7 +82,6 @@ fn logout(cookies: &CookieJar<'_>) -> Redirect {
 fn rocket() -> _ {
     rocket::build()
         .attach(Template::fairing())
-        // .attach(StocksDbConn::fairing())
         .attach(static_resources_initializer!(
             "favicon" => "static/favicon.ico",
         ))
